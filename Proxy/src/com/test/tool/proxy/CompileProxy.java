bc0e39d284d32ad03de02569ccc9e9bc6b5c28e6
lyjnice <1582508438@qq.com>
2017-12-20 10:15:40 +0800

    create a new compileproxy complete by hand

--- Proxy/src/com/test/tool/proxy/CompileProxy.java
+++ Proxy/src/com/test/tool/proxy/CompileProxy.java
@@ -3,9 +3,20 @@ package com.test.tool.proxy;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+import javax.tools.JavaCompiler;
+import javax.tools.JavaCompiler.CompilationTask;
+import javax.tools.JavaFileObject;
+import javax.tools.StandardJavaFileManager;
+import javax.tools.ToolProvider;
 
 public class CompileProxy{
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, SecurityException, IllegalArgumentException, InvocationTargetException {
 		   String rt = "\r\n";
 		   String str = "package com.test.tool.proxy;" + rt +
 
@@ -31,6 +42,26 @@ public class CompileProxy{
 		   fw.write(str);
 		   fw.flush();
 		   fw.close();
+		   
+		   // 使用JavaCompiler 编译java文件  
+	        JavaCompiler jc = ToolProvider.getSystemJavaCompiler();  
+	        StandardJavaFileManager fileManager = jc.getStandardFileManager(null, null, null);  
+	        Iterable<? extends JavaFileObject> fileObjects = fileManager.getJavaFileObjects(fileName);  
+	        CompilationTask cTask = jc.getTask(null, fileManager, null, null, null, fileObjects);  
+	        cTask.call();  
+	        fileManager.close();  
+	        // 使用URLClassLoader加载class到内存    如果用classLoader要保证class在classpath下
+	        URL[] urls = new URL[] { new URL("file:/" + System.getProperty("user.dir") + "/src/") };  
+	        URLClassLoader cLoader = new URLClassLoader(urls);  
+	        Class<?> c = cLoader.loadClass("com.test.tool.proxy.CompileTimeProxy");  
+	        cLoader.close();  
+	        // 利用class创建实例，反射执行方法  只会load构造函数的参数为空的class文件
+		       /* Object obj = c.newInstance();  
+		        Method method = c.getMethod("disply");  
+		        method.invoke(obj);  */
+	        Constructor<?> ctr = c.getConstructor(Moveable.class);
+	        Moveable m = (Moveable)ctr.newInstance(new Tank()); //参数传入被代理对象
+		    m.move();
 	}
   
    
